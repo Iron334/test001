1e6f66b9c79449ef786f5ef904943aeabff21750
Iron334 <61693144+Iron334@users.noreply.github.com>
2020-03-05 10:41:17 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,6 +4,6 @@ $id = $_GET['id'];
 $sql = 'DELETE FROM people WHERE id=:id';
 $statement = $connection->prepare($sql);
 if ($statement->execute([':id' => $id])) {
-  header("Location: /");
+   header("Location: index.php");
 }
 ?>
